86dc92607acd0b51b5925f538d05475cf90457be
yimadaOrz <56173228+yimadaOrz@users.noreply.github.com>
2020-05-17 19:43:06 -0400

    Update FinalC.py

--- BDM_final/FinalC.py
+++ BDM_final/FinalC.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 from pyspark import SparkContext
 from pyspark.sql.session import SparkSession
 import csv
@@ -5,108 +8,74 @@ import numpy as np
 import time 
 import sys
 
+def convert_house(h):
+    if '-' in h:
+        t = h.split('-')
+        assert len(t) == 2
+        house = int(t[0]) + int(t[1])/10**8
+        flag = int(t[1])
+    else:
+        house = float(h)
+        flag = int(house)
+    return (house, flag)
 
-def preprocessing(pid, records):
-    
+def processCenterline(pid, records):
+    # Skip the header
     if pid==0:
         next(records)
-    
-    boro_dict = {'1': 'Manhattan', 
-                 '2': 'Bronx', 
-                 '3': 'Brooklyn', 
-                 '4': 'Queens', 
-                 '5': 'Staten Island'}   
-    
     reader = csv.reader(records)
     
     for row in reader:
-        # Lower street name
-        full_street = row[28].lower()
-        street_label = row[10].lower()
-        # Change boro code into boro name
-        boro = boro_dict[row[13]]
-        
-        for i in [2, 3, 4, 5]:
-            if row[i]:
-                # Check if the house number is in group like '54-098'
-                if row[i].isdigit():
-                    # Transfer number like '098' to '98'
-                    row[i] = float(row[i])
-                    # Group number as integer and house number as decimal 
-                       
-                else:
-                    # Split the group and do the same 
-                    first, row[i] = row[i].split('-')
-                    row[i] = str(int(row[i]))
-                    row[i] = float(first+'.'+row[i])   
-            else:
-                # If it's null, make it 0.0, SQL could only handle with the same type
-                row[i] = 0.0
-        
-        # yield twice, one for the left and the other for the right
-        if street_label == full_street:
-            yield (row[0], street_label, boro, row[2], row[3], 1)
-            yield (row[0], street_label, boro, row[4], row[5], 0)
-        else:
-            yield (row[0], street_label, boro, row[2],  row[3], 1)
-            yield (row[0], street_label, boro, row[4], row[5], 0)
-            yield (row[0], full_street, boro, row[2],  row[3], 1)
-            yield (row[0], full_street, boro, row[4], row[5], 0)
-            
-            
-def dataprocessing(pid, records):
-    
-    if pid==0:
-        next(records)
-    
-    county_dict = {
-        'MAN': 'Manhattan', 'MH':'Manhattan', 'MN': 'Manhattan', 'NEWY': 'Manhattan', 
-        'NEW': 'Manhattan', 'Y': 'Manhattan', 'NY': 'Manhattan', 'BRONX': 'Bronx', 
-        'BX': 'Bronx', 'BK': 'Brooklyn', 'K': 'Brooklyn', 'KING': 'Brooklyn', 'KINGS': 'Brooklyn',
-        'Q': 'Queens', 'QN': 'Queens', 'QNS': 'Queens', 'QU': 'Queens', 'QUEEN': 'Queens',
-        'R': 'Staten Island', 'RICHMOND': 'Staten Island'
-    } 
-    
-    reader = csv.reader(records)
-    
-    for row in reader:
-        
         try:
-            year = int(str(row[4][-4:]))
+            pysicalID = int(row[0])
+            street_label = row[10].lower()
+            full_street = row[28].lower()
+            boro = int(row[13])
+            l_low, _ = convert_house(row[2])
+            l_high, _ = convert_house(row[3])
+            r_low, _ = convert_house(row[4])
+            r_high, _ = convert_house(row[5])
+        except:
+            (pysicalID, street_label, full_street, boro, l_low, l_high, r_low, r_high) = (int(row[0]), '', '', -1, -1.0, -1.0, -1.0, -1.0)
+        if street_label == full_street:
+            yield (pysicalID, street_label, boro, l_low, l_high, 1)
+            yield (pysicalID, street_label, boro, r_low, r_high, 0)
+        else:
+            yield (pysicalID, street_label, boro, l_low, l_high, 1)
+            yield (pysicalID, street_label, boro, r_low, r_high, 0)
+            yield (pysicalID, full_street, boro, l_low, l_high, 1)
+            yield (pysicalID, full_street, boro, r_low, r_high, 0)
+
+
+def processViolation(pid, records):
+    county2idx = {'MAN':1,'MH':1,'MN':1,'NEWY':1,'NEW Y':1,'NY':1,\
+                  'BRONX':2,'BX':2,'PBX':2,\
+                  'BK':3,'K':3,'KING':3,'KINGS':3,\
+                  'Q':4,'QN':4,'QNS':4,'QU':4,'QUEEN':4,\
+                  'R':5,'RICHMOND':5}
+    # Skip the header
+    if pid==0:
+        next(records)
+    reader = csv.reader(records)
+    for row in reader:
+        try:
+            year = int(row[4][-4:])
+            if year not in range(2015,2020):
+                raise ValueError
+            house_str = row[23]
+            house, flag = convert_house(house_str)
+            street = row[24].lower()
+            if row[21] in county2idx.keys():
+                boro = county2idx[row[21]]
+            else:
+                raise ValueError
+            is_left = flag%2
         except:
             continue
-            
-        street = row[24].lower()
-        # Transfer county name into boro name
-       
-        if row[21] and (row[21] in county_dict.keys()):
-                boro = county_dict[row[21]]
-        else:
-            continue
-        
-        
-        if row[23]:
-            if row[23].isdigit():
-                # Check if the place is on the right or left 
-                is_left = int(row[23]) % 2
-                house_number = float(row[23])
-                
-            else:
-                # Some times the house number is like 'W', we skip it
-                try:
-                    first, house_number = row[23].split('-')
-                    house_number = str(int(house_number))
-                    is_left = int(house_number) % 2
-                    house_number = float(first+'.'+house_number)
-                        
-                except:
-                    continue
-        else:
-            continue
-                
-        yield (year, street, boro, house_number, is_left)
-        
-def formatprocessing(records):
+        yield (year, house, street, boro, is_left)
+
+
+def processformat(records):
     for r in records:
         if r[0][1]==2015:
             yield (r[0][0], (r[1], 0, 0, 0, 0))
@@ -118,20 +87,34 @@ def formatprocessing(records):
             yield (r[0][0], (0, 0, 0, r[1], 0))
         elif r[0][1]==2019:
             yield (r[0][0], (0, 0, 0, 0, r[1]))
-        else: 
+        else:
             yield (r[0][0], (0, 0, 0, 0, 0))
+
 
 def compute_ols(y, x=list(range(2015,2020))):
     x, y = np.array(x), np.array(y)
+    # number of observations/points 
     n = np.size(x)
+    # mean of x and y vector 
     m_x, m_y = np.mean(x), np.mean(y)
- 
-    Y = np.sum(y*x) - n*m_y*m_x 
-    X = np.sum(x*x) - n*m_x*m_x
- 
-    coef = Y / X
-    return float(str(coef))
+    # calculating cross-deviation and deviation about x 
+    SS_xy = np.sum(y*x) - n*m_y*m_x 
+    SS_xx = np.sum(x*x) - n*m_x*m_x
+    # calculating regression coefficients 
+    coef = SS_xy / SS_xx
+    return coef
 
+
+def process(i):
+    if ',' in str(i):
+        return "\"{}\"".format(i)
+    else:
+        return str(i)
+
+
+def to_csv(rdd):
+    li = map(process, rdd)
+    return ','.join(li)
 
 if __name__ == "__main__":
     start_time = time.time()
@@ -142,6 +125,7 @@ if __name__ == "__main__":
 
     centerline = sc.textFile('hdfs:///tmp/bdm/nyc_cscl.csv') \
     .mapPartitionsWithIndex(preprocessing)
+    
     violations = sc.textFile('hdfs:///tmp/bdm/nyc_parking_violation/') \
     .mapPartitionsWithIndex(dataprocessing)
     
@@ -158,8 +142,8 @@ if __name__ == "__main__":
             .reduceByKey(lambda x,y: (x[0]+y[0], x[1]+y[1], x[2]+y[2], x[3]+y[3], x[4]+y[4])) \
             .sortByKey() \
             .mapValues(lambda y: y + (compute_ols(y=list(y)),)) \
-            .map(lambda x: ((x[0],) + x[1])) \
-            .map(lambda x: (str(x)[1:-1])) \
-            .saveAsTextFile(output)
-                 
+           .map(lambda x: ((x[0],)+x[1]))\
+           .map(to_csv)\
+           .saveAsTextFile(output)
+    
     print('total running time : {} seconds'.format(time.time()-start_time))
